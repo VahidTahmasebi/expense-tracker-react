d168021a2f81569b6d3b49d37b1f03afb4a5e06e
Vahid Tahmasebi <vahid.tahmasebi95@gmail.com>
2022-12-31 14:35:40 +0330

    Add receive value in TransActionForm file

--- src/components/TransActionForm.js
+++ src/components/TransActionForm.js
@@ -1,14 +1,49 @@
 import { useState } from "react";
 
 const TransActionForm = () => {
+  const [formValue, setFormValue] = useState({
+    type: "Expense",
+    amount: 0,
+    desc: "",
+  });
+
+  // receive enw state
+  const changeHandler = (e) => {
+    setFormValue({ ...formValue, [e.target.name]: e.target.value });
+  };
+
   return (
     <form>
-      <input type='text' name='desc' onChange={changehandler} />
-      <input type='number' name='value' onChange={changehandler} />
+      <input
+        type='text'
+        name='desc'
+        onChange={changeHandler}
+        value={formValue.desc}
+      />
+      <input
+        type='number'
+        name='amount'
+        onChange={changeHandler}
+        value={formValue.amount}
+      />
       <div>
-        <input type='radio' value='expense' id='expense' name='type' />
+        <input
+          type='radio'
+          value='expense'
+          id='expense'
+          name='type'
+          checked={formValue.type === "expense"}
+          onChange={changeHandler}
+        />
         <label for='expense'>Expense</label>
-        <input type='radio' value='income' id='income' name='type' />
+        <input
+          type='radio'
+          value='income'
+          id='income'
+          name='type'
+          checked={formValue.type === "income"}
+          onChange={changeHandler}
+        />
         <label for='income'>Income</label>
       </div>
       <button>add transaction</button>
